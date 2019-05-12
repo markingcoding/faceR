1e2e3b5ee83f7f22b79dbce0a5fe5811472a3570
lyx <a370085093@gmail.com>
2019-05-12 10:28:17 +0900

    change to flask socketio

---
+++ flaskServer.py
@@ -0,0 +1,46 @@
+from importlib import import_module
+import os
+from flask import Flask, render_template, Response, request
+from flask import Flask, render_template
+from flask_socketio import SocketIO, emit
+
+#from camera import Camera
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    """Video streaming home page."""
+    return render_template('index.html')
+
+#def gen(camera):
+#    """Video streaming generator function."""
+#     while True:
+#         frame = camera.get_frame()
+#         yield (b'--frame\r\n'
+#                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+#
+# @app.route('/video_feed', methods=["GET", "POST"])
+# def video_feed():
+#     """Video streaming route. Put this in the src attribute of an img tag."""
+#     return Response(gen(Camera()),
+#                   mimetype='multipart/x-mixed-replace; boundary=frame')
+
+#app.config['SECRET_KEY'] = 'secret!'
+socketio = SocketIO(app)
+
+@socketio.on('message', namespace='/message')
+def test_message(message):
+    emit('imgback_'+request.sid, {'data': message})
+
+@socketio.on('connect', namespace='/message')
+def test_connect():
+    print(request.sid + "connected")
+
+if __name__ == '__main__':
+    debug=True
+    threaded=True
+    app.run(host="0.0.0.0", port=5000)
+    socketio.run(app, host='0.0.0.0', port=5000, debug=True)
+
+
