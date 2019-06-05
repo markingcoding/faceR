72669523fe359f5e3df53f6ff14768d6bc3ad28e
FangDehui <javaeecoding@gmail.com>
2019-06-06 08:58:02 +0900

    change port

--- flaskServer.py
+++ flaskServer.py
@@ -40,7 +40,7 @@ def test_connect():
 if __name__ == '__main__':
     debug=True
     threaded=True
-    app.run(host="0.0.0.0", port=5000)
-    socketio.run(app, host='0.0.0.0', port=5000, debug=True)
+    app.run(host="0.0.0.0", port=8080)
+    socketio.run(app, host='0.0.0.0', port=8080, debug=True)
 
 
